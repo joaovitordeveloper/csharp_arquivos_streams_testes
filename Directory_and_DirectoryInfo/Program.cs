00dd16371e6c5880d40e240b9b229cd22fcea22e
João Vitor Pimentel <joao.pimentel@rbmweb.com.br>
2022-07-19 13:39:12 -0300

    Subindo diretorios

---
+++ Directory_and_DirectoryInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var path = Path.Combine(Environment.CurrentDirectory, "globo");
+//criando um diretorio
+
+var dirGlob = Directory.CreateDirectory(path);
+var dirAmCentral = dirGlob.CreateSubdirectory("América central");
+var dirAmNorte = dirGlob.CreateSubdirectory("América do norte");
+var dirAmSul = dirGlob.CreateSubdirectory("América do sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("Mexico");
+dirAmNorte.CreateSubdirectory("Canada");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panama");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Paraguai");
