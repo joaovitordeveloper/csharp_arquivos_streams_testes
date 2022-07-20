281b6ebbe669f73b9662b0220fb8fa29fd12b1a1
João Vitor Pimentel <joao.pimentel@rbmweb.com.br>
2022-07-20 17:54:18 -0300

    subindo cod

--- Directory_and_DirectoryInfo/Program.cs
+++ Directory_and_DirectoryInfo/Program.cs
@@ -1,19 +1,81 @@
 ﻿
-var path = Path.Combine(Environment.CurrentDirectory, "globo");
-//criando um diretorio
+CriarDiretorioGlobo();
+CriarArquivo();
 
-var dirGlob = Directory.CreateDirectory(path);
-var dirAmCentral = dirGlob.CreateSubdirectory("América central");
-var dirAmNorte = dirGlob.CreateSubdirectory("América do norte");
-var dirAmSul = dirGlob.CreateSubdirectory("América do sul");
+var origem = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "América do sul", "brasil", "brasil.txt");
 
-dirAmNorte.CreateSubdirectory("USA");
-dirAmNorte.CreateSubdirectory("Mexico");
-dirAmNorte.CreateSubdirectory("Canada");
+//MoverArquivo(origem, destino);
+CopiarArquivo(origem, destino);
 
-dirAmCentral.CreateSubdirectory("Costa Rica");
-dirAmCentral.CreateSubdirectory("Panama");
+static void CopiarArquivo(string pathOrigem, string pathDestino)
+{
+  // if (!File.Exists(pathOrigem))
+  // {
+  //   System.Console.WriteLine("O arquivo de origem não existe:");
+  //   return;
+  // }
 
-dirAmSul.CreateSubdirectory("Brasil");
-dirAmSul.CreateSubdirectory("Argentina");
-dirAmSul.CreateSubdirectory("Paraguai");
+
+  // if (File.Exists(pathDestino))
+  // {
+  //   System.Console.WriteLine("O arquivo ja existe no destino:");
+  //   return;
+  // }
+
+  File.Copy(pathOrigem, pathDestino);
+}
+
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+  if (!File.Exists(pathOrigem))
+  {
+    System.Console.WriteLine("O arquivo de origem não existe:");
+    return;
+  }
+
+
+  if (File.Exists(pathDestino))
+  {
+    System.Console.WriteLine("O arquivo ja existe no destino:");
+    return;
+  }
+
+  File.Move(pathOrigem, pathDestino);
+}
+
+static void CriarArquivo()
+{
+  var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+  if (!File.Exists(path))
+  {
+    using var sw = File.CreateText(path);
+    sw.WriteLine("População: 200MM");
+    sw.WriteLine("IDH: 0,901");
+    sw.WriteLine("Dados atualizados em: 20/05/2021");
+  }
+}
+
+static void CriarDiretorioGlobo()
+{
+  var path = Path.Combine(Environment.CurrentDirectory, "globo");
+  //criando um diretorio
+  if (!Directory.Exists(path))
+  {
+    var dirGlob = Directory.CreateDirectory(path);
+    var dirAmCentral = dirGlob.CreateSubdirectory("América central");
+    var dirAmNorte = dirGlob.CreateSubdirectory("América do norte");
+    var dirAmSul = dirGlob.CreateSubdirectory("América do sul");
+
+    dirAmNorte.CreateSubdirectory("USA");
+    dirAmNorte.CreateSubdirectory("Mexico");
+    dirAmNorte.CreateSubdirectory("Canada");
+
+    dirAmCentral.CreateSubdirectory("Costa Rica");
+    dirAmCentral.CreateSubdirectory("Panama");
+
+    dirAmSul.CreateSubdirectory("Brasil");
+    dirAmSul.CreateSubdirectory("Argentina");
+    dirAmSul.CreateSubdirectory("Paraguai");
+  }
+}
