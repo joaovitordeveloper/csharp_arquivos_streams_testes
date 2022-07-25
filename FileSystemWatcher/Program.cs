d9a6328cae11def19caa04024a15d46d319848b2
João Vitor Pimentel <joao.pimentel@rbmweb.com.br>
2022-07-25 13:42:52 -0300

    novos cod de estudos

---
+++ FileSystemWatcher/Program.cs
@@ -0,0 +1,35 @@
+﻿
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var path = @"C:\xampp\htdocs\csharp_arquivos_streams_testes\Directory_and_DirectoryInfo\globo";
+    using var fsw = new FileSystemWatcher(path);
+
+    fsw.Created += OnCreated;
+    fsw.Deleted += OnDeleted;
+    fsw.Renamed += OnRenamed;
+
+    fsw.EnableRaisingEvents = true;
+    fsw.IncludeSubdirectories = true;
+
+    System.Console.WriteLine("monitoramento de eventos na pasta Globo");
+    System.Console.WriteLine("Pressione [enter] para finalizar");
+    System.Console.console.ReadLine();
+
+    void OnCreated(object sender, FileSystemEventArgs e)
+    {
+      System.Console.WriteLine($"foi criado o arquivo {e.Name}");
+    }
+
+    void OnDeleted(object sender, FileSystemEventArgs e)
+    {
+      System.Console.WriteLine($"foi excluido o arquivo {e.Name}");
+    }
+
+    void OnRenamed(object sender, FileSystemEventArgs e)
+    {
+      System.Console.WriteLine($"O arquivo {e.OldName} foi renomeado para {e.Name}");
+    }
+  }
+}
---
+++ LerDiretorios/Program.cs
@@ -0,0 +1,44 @@
+﻿
+var path = @"C:\xampp\htdocs\csharp_arquivos_streams_testes\Directory_and_DirectoryInfo\globo";
+//LerDiretorios(path);
+LerArquivos(path);
+
+System.Console.WriteLine("Digite enter para finalizar...");
+System.Console.ReadLine();
+
+static void LerDiretorios(string path)
+{
+
+  if (Directory.Exists(path))
+  {
+    var diretorios = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+    foreach (var dir in diretorios)
+    {
+      var dirInfo = new DirectoryInfo(dir);
+      System.Console.WriteLine($"[Nome]:{dirInfo.Name}");
+      System.Console.WriteLine($"[Raiz]:{dirInfo.Root}");
+      if (dirInfo.Parent != null)
+        System.Console.WriteLine($"[Pai]:{dirInfo.Parent.Name}");
+      System.Console.WriteLine("----------------------------------");
+    }
+  }
+  else
+  {
+    System.Console.WriteLine($"{path} não existe");
+  }
+
+}
+
+static void LerArquivos(string path)
+{
+  var arquivos = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+  foreach (var arquivo in arquivos)
+  {
+    var fileInfo = new FileInfo(arquivo);
+    System.Console.WriteLine($"[Nome]:{fileInfo.Name}");
+    System.Console.WriteLine($"[Tamanho]:{fileInfo.Length}");
+    System.Console.WriteLine($"[Ultimo acesso]:{fileInfo.LastAccessTime}");
+    System.Console.WriteLine($"[Pasta]:{fileInfo.DirectoryName}");
+    System.Console.WriteLine("-----------------------------------");
+  }
+}
